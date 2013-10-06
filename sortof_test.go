6baae389b688ac7095014335847b73c2947a51e3
Vytautas Šaltenis <vytas@rtfb.lt>
2013-10-06 08:47:23 +0300

    Add test case for ints, still exercises stdlib

---
+++ sortof_test.go
@@ -0,0 +1,32 @@
+package sortof
+
+import (
+    "fmt"
+    "sort"
+    "testing"
+)
+
+func TestInts(t *testing.T) {
+    var cases = []struct {
+        arr      []int
+        expected string
+    }{
+        {arr: []int{}, expected: "[]"},
+        {arr: []int{1}, expected: "[1]"},
+        {arr: []int{2, 1}, expected: "[1 2]"},
+        {arr: []int{1, 2}, expected: "[1 2]"},
+        {arr: []int{1, 2, 3}, expected: "[1 2 3]"},
+        {arr: []int{2, 1, 3}, expected: "[1 2 3]"},
+        {arr: []int{3, 2, 1}, expected: "[1 2 3]"},
+        {arr: []int{5, 6, 333, 8}, expected: "[5 6 8 333]"},
+        {arr: []int{123, 124, 125, 126}, expected: "[123 124 125 126]"},
+    }
+    for i, c := range cases {
+        sort.Ints(c.arr)
+        got := fmt.Sprintf("%v", c.arr)
+        if got != c.expected {
+            t.Fatalf("Test case %d failed.\nExpected: %v\nGot: %v\n",
+                i, c.expected, got)
+        }
+    }
+}
