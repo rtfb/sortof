c88d6545fdd399e247ff2cb43030c2121735ff4b
Vytautas Šaltenis <vytas@rtfb.lt>
2021-11-06 21:56:03 +0200

    Reencode an added mp4 file into a portrait orientation
    This app will be watching a specific folder for uploaded files in
    portrait orientation (as sometimes happens on the phone) and will use
    ffmpeg to rotate it into a larger landscape video with the portrait in
    the middle.

--- docker/video-autoconvert/app.py
+++ docker/video-autoconvert/app.py
@@ -1,8 +1,11 @@
 import logging
 import os
 import random
+import shutil
+import subprocess
 import sys
 import time
+from datetime import datetime, timedelta
 
 from watchdog.events import FileCreatedEvent, FileSystemEventHandler
 from watchdog.observers import Observer
@@ -13,11 +16,45 @@ DIR_TO_WATCH = '/autoconvert/'
 recent_events = []
 
 
+def ensure_dir(dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+
+def ffmpeg(input_path):
+    logging.info('inside ffmpeg')
+    start = datetime.now()
+    basename = os.path.basename(input_path)
+    tmp_dir = os.path.join(DIR_TO_WATCH, 'tmp')
+    ensure_dir(tmp_dir)
+    tmp_path = os.path.join(tmp_dir, basename)
+    vf = '"pad=width=3412:height=1920:x=1166:y=0:color=black"'
+    other_flags = '-nostats -preset veryfast -codec:a copy'
+    cmd = 'ffmpeg -i {} -vf {} {} {}'.format(
+        input_path, vf, other_flags, tmp_path)
+    logging.info(cmd)
+    process = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True)
+    stdout, stderr = process.communicate()
+    if stdout:
+        logging.info('ffmpeg stdout: ' + str(stdout))
+    if stderr:
+        logging.info('ffmpeg stderr: ' + str(stderr))
+    logging.info('duration: ' + str(datetime.now() - start))
+    output_path = os.path.join('/video', basename)
+    output_tmp = output_path + '.tmp'
+    shutil.move(tmp_path, output_tmp)
+    os.rename(output_tmp, output_path)
+
+
 class Watcher(FileSystemEventHandler):
     def on_created(self, event):
         if type(event) == FileCreatedEvent:
             file = event.src_path
             recent_events.append('New file: ' + file + ', ' + os.path.basename(file))
+            if file.endswith('.mp4'):
+                ffmpeg(file)
         else:
             recent_events.append(str((type(event))))
 
@@ -26,7 +63,7 @@ def monitor():
     path = DIR_TO_WATCH
     e_handler = Watcher()
     watch = Observer()
-    watch.schedule(e_handler, path, recursive=True)
+    watch.schedule(e_handler, path, recursive=False)
     watch.start()
     return watch
 
@@ -48,7 +85,8 @@ def hello():
 
 
 def main():
-    logging.log(logging.WARN, 'inside main()')
+    logging.basicConfig(level=logging.INFO)
+    logging.info('inside main()')
     recent_events.append('this is a test')
     watch = monitor()
     # watch.stop()
