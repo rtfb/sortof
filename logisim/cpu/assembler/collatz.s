92abb0407df308d483df700aa721414d2c032e6d
Vytautas Šaltenis <vytas@rtfb.lt>
2022-12-18 15:07:22 +0200

    Rewrite collatz program with the new jumps to labels
    The program is now half as long and much more readable, yay!

--- logisim/cpu/assembler/collatz.s
+++ logisim/cpu/assembler/collatz.s
@@ -7,54 +7,20 @@ ld r7    // r7 = input number
 
 li 0
 xor r7   // will set Z flag if r7 == acc == 0
-getst r6
-getpc r5
-li 6
-add r5
-setst r6
-jnz       // jump over the halt to the actual code if precondition was good
+jnz proceed  // jump over the halt to the actual code if precondition was good
 halt
 
+proceed:
 li 1
 getacc r6    // hold the index in r6
 
 //
-// calculate jump addresses and store them in r0, etc
-//
-
-// set r0 to begin_loop
-li 3
-shli 3
-getacc r0
-li 7
-or r0
-getacc r0   // r0=31
-
-// set r1 to even case
-li 5
-shli 3
-getacc r1
-li 6
-or r1
-getacc r1  // r1=46
-
-// set r2 to after_loop
-li 7
-shli 3
-getacc r2
-li 3
-or r2
-getacc r2   // r2=59
-
-//
 // THE LOOP
 //
-
-// begin_loop:
-li 1
-and r7
-setacc r1
-jz        // jump if r7 was even
+begin_loop:
+  li 1
+  and r7
+  jz  even_case       // jump if r7 was even
 
   // odd case:
   setacc r7
@@ -67,11 +33,9 @@ jz        // jump if r7 was even
 
   inc 1  // index++
   getacc r6
+  jmp  begin_loop  // loop back
 
-  setacc r0
-  jmp   // loop back
-
-  // even case:
+even_case:
   setacc r7
   shri 1
   getacc r7
@@ -82,11 +46,9 @@ jz        // jump if r7 was even
 
   li 1
   xor r7
-  setacc r2
-  jz      // jump to end of program
+  jz after_loop   // jump to end of program
 
-  setacc r0
-  jmp   // loop back
+  jmp begin_loop  // loop back
 
-// after_loop:
+after_loop:
 halt
